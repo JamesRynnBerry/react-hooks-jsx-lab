8f3b07594c6854a2e408dca5b4da425ae452d4b6
James Rynn Berry <jb5260@nyu.edu>
2021-06-28 16:53:17 -0400

    1/11 Passing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
+  <div id="home">
+    <h1 style={{ color : "firebrick"  }}>
+    { name } is a Web Developer from your { city }
+    </h1>
+  </div>
   return <div>Home</div>;
 }
 
